c87adf1ebdeb23e6e0be8f71dfc2df9471f9c59a
Orange Ass <fayicug@autowb.com>
2018-11-19 04:05:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ function currentLine(katzDeliLine) {
   for (var i = 0; i < katzDeliLine.length; ++i) {
     status = status + `${i+1}. ${katzDeliLine[i]}, `;
   }
-  
+
+  status.pop();
   return status;
 }
