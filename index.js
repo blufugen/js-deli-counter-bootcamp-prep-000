03161a75f059797426d3b363cd63fd32b03c1b8f
Orange Ass <fayicug@autowb.com>
2018-11-19 04:05:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function currentLine(katzDeliLine) {
     return "The line is currently empty.";
   }
   
-  var status = "The line is currently ";
+  var status = "The line is currently: ";
   for (var i = 0; i < katzDeliLine.length; ++i) {
     status = status + `${i+1}. ${katzDeliLine[i]} `;
   }
