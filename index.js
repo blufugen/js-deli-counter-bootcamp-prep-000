691052d19deb7facbb4eb23bb4526aa7c037efc8
Orange Ass <fayicug@autowb.com>
2018-11-19 04:00:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber(katzDeliLine, name) {
-  katzDeliLine.push[name];
+  katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.length + 1} in line.`;
 }
 
