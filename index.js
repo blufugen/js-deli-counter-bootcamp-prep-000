a67b0bbdb0550c6187a7df1ea67942d01fd14b88
Orange Ass <fayicug@autowb.com>
2018-11-19 03:46:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function nowServing(katzDeliLine) {
 
 function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
-    return "There is nobody waiting to be served!";
+    return "The line is currently empty.";
   }
   
   var status = ""
